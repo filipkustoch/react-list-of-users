4948460958cd0c16cbb10731d981b2dad18d3630
filipkustoch <filip.kustoch1@gmail.com>
2022-11-28 17:04:36 +0100

    Added validation logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,6 +10,10 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) return;
     console.log(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
@@ -27,9 +31,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label>Username</label>
-        <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler} />
+        <input
+          type="text"
+          id="username"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label>Age (Years)</label>
-        <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler} />
+        <input
+          type="number"
+          id="age"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
